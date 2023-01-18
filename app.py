204c8484024df5c360035019ae3cefcf82ff4159
samwesley <samwesley3@gmail.com>
2023-01-17 21:10:44 -0800

    incorporating flask and requirements

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import orchestrator
 
 app = Flask(__name__)
 
 @app.route('/health')
 def health():
     return "OK", 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- new_subscription.py
+++ new_subscription.py
@@ -1,8 +1,10 @@
 import json
 import variables
+import random
+import string
 
 
-def newSubscription(userEmail, slackUsername, categories):
+def newSubscription(userEmail, categories, category_details):
     with open("users/subscriptions.json") as f:
         data = json.load(f)
         #look through all user id's and add a new id for this user that's one larger than the largest id
@@ -11,5 +13,17 @@ def newSubscription(userEmail, slackUsername, categories):
             ids.append(user["id"])
         newId = max(ids) + 1
 
-        data["users"].append({"id" : newId, "email": userEmail, "categories": categories, "slack": {"username": slackUsername}})
+        data["users"].append({"id" : newId, "email": userEmail, "categories": categories, "category_details": category_details})
         print("New user added! Email: " + userEmail + " Categories: " + categories)
+        with open("users/subscriptions.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+code_list = []
+
+def generate_code(input):
+    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(1,3)))
+    if code not in code_list:
+        code_list.append(code)
+        return code
+    else:
+        return generate_code(input)
